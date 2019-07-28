1fb1d4f590ef3d587d7fdc5a32f3774003d6da6e
rushilnpatel <rushilnpatel@gmail.com>
2019-07-28 14:36:01 -0400

    Test Material Design

--- playground/src/app/app.module.ts
+++ playground/src/app/app.module.ts
@@ -4,6 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+
 
 @NgModule({
   declarations: [
@@ -12,9 +15,12 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  exports: [MatButtonModule, MatCheckboxModule],
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
