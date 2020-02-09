a8cb661b1af10d75c2b7588ee0407d7177a5b129
Rushil Patel <rushilnpatel@gmail.com>
2020-02-09 14:42:36 -0500

    Removed WFS, Need to create Seperate Playground Library

--- playground/src/environments/environment.prod.ts
+++ playground/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  breakpoints: [375, 768, 1024, 1440],
-  iconsBasePath: 'assets/icons/'
+  // breakpoints: [375, 768, 1024, 1440],
+  // iconsBasePath: 'assets/icons/'
 };
