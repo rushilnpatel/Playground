c7535fa593c735f56553ee842c0d917cf104b101
Rushil Patel <rushilnpatel@gmail.com>
2020-05-23 14:30:58 -0400

    Angular 9 upgrade

--- playground/src/app/app.module.ts
+++ playground/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
-import { AgGridModule } from 'ag-grid-angular';
+// import { AgGridModule } from 'ag-grid-angular';
 import { SideNavModule } from './side-nav/side-nav.module';
 
 // Firebase Setup
@@ -24,7 +24,7 @@ import { environment } from 'src/environments/environment';
     AppRoutingModule,
     BrowserAnimationsModule,
     // MaterialModule, // load this on deamnd with the module so that performance gets increase
-    AgGridModule.withComponents([]),
+    // AgGridModule.withComponents([]),
     SideNavModule
   ],
   providers: [],
