ac58fa16cf038e2ea4a89b27a53a0e8540591f8e
Rushil Patel <rushilnpatel@gmail.com>
2020-05-25 18:46:03 -0400

    Reactive Form

--- playground/src/app/register/register.component.ts
+++ playground/src/app/register/register.component.ts
@@ -11,13 +11,42 @@ import { FormArray, FormGroup } from '@angular/forms';
 export class RegisterComponent implements OnInit {
 
 registerFormGroup: FormGroup;
+address = [
+  {label: 'Primary Residence', value: 'Primary Residence'},
+  {label: 'Permenant Residence', value: 'Permenant Residence'},
+  {label: 'Future Residence', value: 'Future Residence'}
+];
 
   constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
     this.registerFormGroup = this.fb.group({
-      firstName: ['']
+      firstName: ['', Validators.required],
+      lastName: null,
+      email : null,
+      password: [null, Validators.required],
+      sendCatalog: true,
+      address: ['']
     })
+  }
+  clicked() {
+
+    /* setValue Method to use the set Values of all form Controls */
+    /* this.registerFormGroup.setValue({
+      firstName: 'Rushil',
+      lastName: 'Patel',
+      email: 'rushilnpa',
+      sendCatalog: false
+    }) 
+    /*
+
+    /* patchValue to set Value for any one formControl */ 
+    this.registerFormGroup.patchValue({
+      firstName: 'Sepu'
+    })
+
+   const x =  this.registerFormGroup.get('password');
+   console.log('x', x)
   }
 
 }
